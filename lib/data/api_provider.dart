104f7a8c8153a4cb72ddb4f11fa2e66d8eba3265
Iqbal-Maulana67 <fikri.rid@gmail.com>
2022-11-15 19:27:01 +0700

    Final Commit

---
+++ lib/data/api_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:flutter_rest_api/model/popular_movies.dart';
+
+class ApiProvider {
+  String apiKey = '13e46f5e5736bfd93514cfe34af54882';
+  String baseUrl = 'https://api.themoviedb.org/3';
+
+  Client client = Client();
+
+  Future<PopularMovies> getPopularMovies() async {
+    // String url = '$baseUrl/movie/popular?api_key=$apiKey';
+    // print(url);
+    Response response =
+        await client.get(Uri.parse('$baseUrl/movie/popular?api_key=$apiKey'));
+
+    if (response.statusCode == 200) {
+      return PopularMovies.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+}
